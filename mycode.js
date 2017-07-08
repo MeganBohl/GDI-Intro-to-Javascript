0234a85879ce0a342ea02354bd51b041d205177f
MeganBohl <meganbohl@outlook.com>
2017-07-08 10:49:30 -0500

    Add files via upload

---
+++ mycode.js
@@ -0,0 +1,19 @@
+//alert('Hello World!');
+console.log('Secret message');
+
+var favoriteColor = 'purple';
+var favoriteNumber = 8;
+
+console.log(favoriteColor);
+alert(favoriteNumber);
+
+var price1 = 20;
+var price2 = 30;
+var total = price1 + price2;
+var mod=price2%price1
+
+console.log(total);
+alert(mod)
+
+var kittensName = 'Fluffy';
+console.log('I\'d like to use an apostrophe');
